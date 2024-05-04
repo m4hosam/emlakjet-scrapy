33ad2e14ed7345d7887f43d2df38c6a054f8dcd2
Mohamed Hosam <mohamedhosaam154@gmail.com>
2024-05-04 19:06:52 +0300

    removing sh file

--- proje.sh
+++
@@ -1,8 +0,0 @@
-# python -m venv venv
-# .\venv\Scripts\activate
-
-# source ./venv/Scripts/activate
-scrapy runspider emlak_scrapy.py
-python veri_analizi.py
-python veri_temizleme.py
-python train_model.py
